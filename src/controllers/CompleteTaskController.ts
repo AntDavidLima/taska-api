8dbf0bd8b8ed34c285aff2affd288f6d55f42f8f
David Lima <antdavidlima@gmail.com>
2023-05-11 10:27:36 -0300

    feat: create complete task endpoint

---
+++ src/controllers/CompleteTaskController.ts
@@ -0,0 +1,21 @@
+import { TaskServiceFactory } from "@/factories/TaskServiceFactory";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export class CompleteTaskController {
+  static async complete(request: FastifyRequest, reply: FastifyReply) {
+    const paramsSchema = z.object({
+      id: z.coerce.number(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const taskService = TaskServiceFactory.make();
+
+    const task = await taskService.update(id, {
+      completedAt: new Date(),
+    });
+
+    return reply.status(200).send(task);
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { TaskController } from "./controllers/TaskController";
+import { CompleteTaskController } from "./controllers/CompleteTaskController";
 
 export async function routes(app: FastifyInstance) {
   app.post("/tasks", TaskController.save);
   app.get("/tasks", TaskController.index);
   app.put("/tasks/:id", TaskController.update);
   app.delete("/tasks/:id", TaskController.destroy);
+
+  app.patch("/tasks/:id/complete", CompleteTaskController.complete);
 }
